614191e29e11f6e1dd9be27ca64af94b1ab96e34
Scaffolder <scaffolder@backstage.io>
2021-07-19 06:55:37 +0000

    Initial commit

---
+++ .ngif/battenberg-install-template.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Purpose of this file is to execute the battenberg install command
+# and feed in the values previously configured in the context file.
+
+template_context_file='.cookiecutter.json'
+template_context_path="./${template_context_file}"
+
+echo "template_context_file:"
+cat "${template_context_path}"
+echo "end template_context_file."
+echo
+
+template_url="${github_base_url}/${template_name}"
+
+echo "Installing ${template_url}"
+echo
+
+# This codeblock answers the prompts issued by battenberg below.
+{
+    if [[ -d "$HOME/.cookiecutters" ]]; then
+        # You've downloaded ... before.
+        # Is it okay to delete and re-download it? [yes]:
+        echo "1";
+        sleep 1;
+    fi
+
+    # template []:
+    echo # accept the default
+    sleep 1;
+
+    # component_id []:
+    jq -r '.component_id' ${template_context_path}
+    sleep 1;
+
+    # java_package_name [javakotlinlibtest*****]:
+    jq -r '.java_package_name' ${template_context_path}
+    sleep 1;
+
+} | battenberg install "${template_url}"
